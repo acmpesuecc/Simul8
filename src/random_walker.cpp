bd96d7bef29bf434954cc476b9c7f2bd60dea944
sherlockhome12 <boraarshia@gmail.com>
2024-10-19 03:40:33 +0530

    updated the cpp random walker file

--- src/random_walker.cpp
+++ src/random_walker.cpp
@@ -1,65 +1,46 @@
 #include <raylib.h>
 
-class Walker{
-
+class Walker {
 private:
-        int x,y;
+    int x, y;
+    int width, height;
 
 public:
+    Walker(int w, int h) : width(w), height(h) {
+        reset();
+    }
 
-        Walker(int width,int height){
-            x = width/2;
-            y = height/2;
-        }
+    void reset() {
+        x = width / 2;
+        y = height / 2;
+    }
 
-        void render(){
-            DrawPixel(x, y, WHITE);
-        }
+    void render() {
+        DrawPixel(x, y, WHITE);
+    }
 
-        void step(){
-            int choice = GetRandomValue(0,3);
-            
-            if(choice == 0){
-                x++;
-            }
-            else if (choice == 1){
-                x--;
-            }
-            else if (choice == 2){
-                y++;
-            }
-            else{
-                y--;
-            }
-        }
+    void step() {
+        int choice = GetRandomValue(0, 3);
         
-
-        
+        if (choice == 0) {
+            x++;
+        } else if (choice == 1) {
+            x--;
+        } else if (choice == 2) {
+            y++;
+        } else {
+            y--;
+        }
+    }
 };
 
-
-int main(void){
+int main(void) {
     const int width = 800;
     const int height = 600;
     InitWindow(width, height, "Random walker :D");
     SetTargetFPS(60);
     
-    Walker w(width,height);
+    Walker w(width, height);
     
-    int i = 1;
-    while(!WindowShouldClose()){
-        w.step();
-        BeginDrawing();
-        w.render();
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
-
-
-}
-
-
-
-
+    while (!WindowShouldClose()) {
+        if (IsKey
